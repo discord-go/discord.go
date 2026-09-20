f0f3a65851e63ce27fded91545fb39bff11f51ff
flawme <flawme@proton.me>
2026-09-20 18:42:38 +0530

    release: v0.14.2-blend

--- http/doc.go
+++ http/doc.go
@@ -4,4 +4,4 @@ package http
 
 // Version is the library version. It is included in the default User-Agent
 // header sent with REST requests so Discord can identify the library.
-const Version = "0.14.1-blend"
+const Version = "0.14.2-blend"
